be5eb05ee8e3d88414fe12dd4d5c0977342457ad
SebastianZasadni <emblelele@gmail.com>
2023-05-11 12:00:53 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Searchbar from './components/SearchBar/Searchbar';
-import ImageGallery from './components/ImageGallery/Imagegallery';
-import ImageGalleryItem from './components/ImageGalleryItem/ImageGalleryItem';
+import {Searchbar} from './components/SearchBar/Searchbar';
+import {ImageGallery} from './components/ImageGallery/Imagegallery';
+import {ImageGalleryItem} from './components/ImageGalleryItem/ImageGalleryItem';
 export const App = () => (
   <>
     <Searchbar />
