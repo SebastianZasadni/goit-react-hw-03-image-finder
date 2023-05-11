89c0ef1eababc5539f6eb1b93412996d9817c640
SebastianZasadni <emblelele@gmail.com>
2023-05-11 11:56:04 +0200

    fiz

--- src/components/ImageGallery/Imagegallery.jsx
+++ src/components/ImageGallery/Imagegallery.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import axios from 'axios';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
@@ -25,31 +24,3 @@ export class ImageGallery extends Component {
     );
   }
 }
-=======
-import React, { Component } from 'react';
-import axios from 'axios';
-import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
-import css from '../../Index.module.css';
-
-export class ImageGallery extends Component {
-  state = {
-    images: [],
-  };
-
-  async componentDidMount() {
-    const response = await axios.get(
-      `https://pixabay.com/api/?q=${localStorage.query}}&page=1&key=34267096-8e89032011ab6ec20673badb0&image_type=photo&orientation=horizontal&per_page=12`
-    );
-    this.setState({ images: response.data.hits });
-  }
-  render() {
-    return (
-      <ul className={css.imagegallery}>
-        {this.state.images.map(i => (
-          <ImageGalleryItem imageGallerySrc={i.previewURL} />
-        ))}
-      </ul>
-    );
-  }
-}
->>>>>>> 48e295da04876d3c37de70d2bcdb1e079fc775d7
--- src/components/SearchBar/Searchbar.jsx
+++ src/components/SearchBar/Searchbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from 'react';
 import css from '../../Index.module.css';
 export class Searchbar extends Component {
@@ -29,35 +28,3 @@ export class Searchbar extends Component {
     );
   }
 }
-=======
-import React, { Component } from 'react';
-import css from '../../Index.module.css';
-export class Searchbar extends Component {
-  makeGallery = evt => {
-    evt.preventDefault();
-    const form = evt.currentTarget;
-    const searchedImages = form.elements.query.value;
-    localStorage.query = searchedImages;
-  };
-  render() {
-    return (
-      <header className={css.searchbar}>
-        <form className={css.searchform} onSubmit={this.makeGallery}>
-          <button type="submit" className={css.searchformbutton}>
-            <span className={css.searchformbuttonlabel}>Search</span>
-          </button>
-          <input
-            name="query"
-            type="text"
-            className={css.searchforminput}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search image and photos"
-            id="search-input"
-          ></input>
-        </form>
-      </header>
-    );
-  }
-}
->>>>>>> 48e295da04876d3c37de70d2bcdb1e079fc775d7
