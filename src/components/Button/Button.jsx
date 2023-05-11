e370360fc18a9a9338a01980adad45b837bcb050
SebastianZasadni <emblelele@gmail.com>
2023-05-11 11:57:26 +0200

    fix

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // const Button = props => {
 //   return (
 // //     <button type="submit" className="SearchForm-button">
@@ -8,14 +7,3 @@
 // };
 
 // export default Button;
-=======
-// const Button = props => {
-//   return (
-// //     <button type="submit" className="SearchForm-button">
-// //       <span className="SearchForm-button-label">Search</span>
-// //     </button>
-//   );
-// };
-
-// export default Button;
->>>>>>> 48e295da04876d3c37de70d2bcdb1e079fc775d7
